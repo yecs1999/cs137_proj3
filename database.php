22fb99adebc92ed06b7cd292dd94301597ef96ee
yecs1999 <chrischromeacc@gmail.com>
2020-05-19 03:40:56 -0700

    netbeans version and finished part of servlet to load main imgs

--- database.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    //run mysqladmin -u root password password in the terminal for XAMPP before this line, or set password to whatever you want and change password below
-    $dbcon = new PDO("mysql:host=localhost;dbname=cars", 'root', 'password');
-?>
